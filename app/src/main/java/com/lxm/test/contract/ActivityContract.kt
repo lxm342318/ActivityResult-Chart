8a5bdccc23fd1ec958d4868a7f5b49546678d578
yimeng <limx@saiwei.com>
2023-03-06 17:49:57 +0800

    优化

--- app/src/main/java/com/lxm/test/contract/ActivityContract.kt
+++ app/src/main/java/com/lxm/test/contract/ActivityContract.kt
@@ -5,19 +5,20 @@ import android.content.Context
 import android.content.Intent
 import androidx.activity.result.contract.ActivityResultContract
 import com.lxm.test.NULL_CHARACTER
+import com.lxm.test.ui.MainActivity
 
 class ActivityContract : ActivityResultContract<String, String>() {
-
-    override fun createIntent(context: Context, input: String?): Intent {
-        val intent = Intent(context, SecondActivity::class.java)
-        intent.putExtra("data", input.toString())
-        return intent
-    }
 
     override fun parseResult(resultCode: Int, intent: Intent?): String {
         val  data  = intent?.getStringExtra("data")
         return if (resultCode == Activity.RESULT_OK && data != null)
             data
         else NULL_CHARACTER
+    }
+
+    override fun createIntent(context: Context, input: String): Intent {
+        val intent = Intent(context, MainActivity::class.java)
+        intent.putExtra("data", input)
+        return intent
     }
 }
